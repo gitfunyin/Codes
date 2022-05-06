049afdc52ae2e00697a3b8b14662f717a8d68657
Fun Yin <goethe3.fy@gmail.com>
2022-05-06 12:15:19 +0800

    add files

---
+++ Readme.md
@@ -0,0 +1,13 @@
+![logo](Hsbclogo.jpeg)
+
+## About this repository
+This is the repository for HSBC Data Science Bootcamp
+
+### Table of contents
+- Intro to DS
+- Git and GitHUB
+
+1. test
+1. test 
+1. test
+
---
+++ simple.py
@@ -0,0 +1,14 @@
+# This a python learning that print hello word!. # in python means to comment code block.
+
+print("hello world")
+print("This is my first python code")
+
+x = 5/3
+
+print("this is a variable {}",format(x))
+
+print("changes")
+
+print("another try")
+
+
